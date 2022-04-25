90b34722d6cd5ad6cd9c553458501e52766cbc29
shantanu kulkarni <shantanu@nulligma.com>
2022-04-25 12:19:21 +0530

    solution complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import keyWords from "./data/ColorKeywords";
+import HexCodes from "./data/HexCodes";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [codes, setCodes] = useState([]);
+
+  useEffect(() => {
+    keyWords.forEach((e, index) => {
+      if (e.findIndex((e) => e === value) !== -1) {
+        setCodes(HexCodes[index]);
+      }
+    });
+  }, [value]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const valueTable = () => {
+    return (
+      <div
+        style={{
+          marginTop: "30px",
+          display: "grid",
+          gridTemplateColumns: "auto auto auto",
+        }}
+      >
+        {codes.map((e, index) => (
+          <div key={index} style={{ padding: "20px", backgroundColor: `${e}` }}>
+            {e}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Color a Word!</p>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input
+            value={value}
+            onChange={onChange}
+            placeholder="Enter color name"
+            type="text"
+            name="colorvalue"
+            required
+          />
+          <button type="submit">search</button>
+          {valueTable()}
+        </div>
+      </form>
     </div>
   );
 }
